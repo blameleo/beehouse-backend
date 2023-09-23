1f9f0b789777eef4246e62abff6af48ca1607421
leonard <leonardgre@gmail.com>
2023-09-23 23:51:05 +0000

    made change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173/"],
+    origin: ["http://127.0.0.1:5173"],
   })
 );
 app.use(express.static("public"));
