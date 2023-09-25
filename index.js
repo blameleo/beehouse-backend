a30a48d8e2008715de795db49cd106eceeec8e02
leonard <leonardgre@gmail.com>
2023-09-25 13:14:43 +0000

    added cors config

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:5173",
+      "http://127.0.0.1:5174",
       "https://beehouse-app-frontend.vercel.app",
     ],
   })
